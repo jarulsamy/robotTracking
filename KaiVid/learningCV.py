cd5ec26ea9015144d8622531aca3f83bea31dc57
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2017-12-02 21:12:40 -0700

    Bug fixes

--- KaiVid/learningCV.py
+++ KaiVid/learningCV.py
@@ -2,8 +2,6 @@ from base.KaicongInput import KaicongInput
 import cv2
 import numpy as np
 
-maxCountour = 0 #DO NOT DELETE ME
-
 ### Variables for click_and_crop ###
 mousePoint = []
 cropping = False
@@ -11,7 +9,9 @@ cropping = False
 
 
 class KaicongVideo(KaicongInput):
-
+    global maxContour, maxContourData
+    maxContour = 0
+    maxContourData = 0
     ### Mouse point Drawing ###
     def click_and_crop(event, x, y, flags, param):
     	global mousePoint, cropping
@@ -22,8 +22,8 @@ class KaicongVideo(KaicongInput):
     		mousePoint.append((x, y))
     		cropping = False
 
-    cv2.namedWindow("Final")
-    cv2.setMouseCallback("Final", click_and_crop)
+    cv2.namedWindow("Original")
+    cv2.setMouseCallback("Original", click_and_crop)
     ### Mouse Point Drawing End ###
 
     PACKET_SIZE = 1024
@@ -68,13 +68,14 @@ if __name__ == "__main__":
         kernel = np.ones((5,5), np.uint8)
 
         readColors = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.IMREAD_COLOR)
+        origPic = readColors
         #HSV Works really well here, currenty sets everything red to white
         #Else set to black
         altColorSpaceImg = cv2.cvtColor(readColors, cv2.COLOR_BGR2YUV) #Converts to YUV
+
         blurredImg = cv2.GaussianBlur(altColorSpaceImg, (11, 11), 10) #Blurs image to deal with noise
         #blurredImg = cv2.medianBlur(blurredImg, 5) #Uses median blur to filter out even more noise
         blurredImg = cv2.bilateralFilter(blurredImg, 25, 75, 75) #Uses bilaterial filtering to deal with more noise
-
         ### Mask Stuff ###
         mask = cv2.inRange(blurredImg, redLower, redUpper)
     	mask = cv2.erode(mask, kernel, iterations=2)
@@ -89,11 +90,15 @@ if __name__ == "__main__":
 
         for contour in contours:
             contourSize = cv2.contourArea(cnt)
-            if contourSize > maxCountour:
+            global maxContour
+            if contourSize > maxContour:
+                print('big')
+                global maxContour, maxContourData
                 maxContour = contourSize
                 maxContourData = contour
 
         areaMask = np.zeros_like(mask)
+
 
         cv2.fillPoly(areaMask,[maxContourData],1) # Draws new areaMask onto new image
 
@@ -106,17 +111,28 @@ if __name__ == "__main__":
         # Contour Stuff End ###
 
         ### Show Images ###
-        cv2.imshow('Mask',mask)
-        cv2.imshow('Alternative Color Space Image', altColorSpaceImg)
-        #cv2.imshow('Final',finalImage)
+        cv2.imshow('Final',finalImage)
+        cv2.imshow('Mask', mask)
 
         if mousePoint == []:
-            cv2.imshow('Final',finalImage)
+            cv2.imshow('Original', origPic)
         else:
-            cv2.rectangle(finalImage, mousePoint[0], mousePoint[1], (0, 255, 0), 2)
-    	cv2.imshow("Final", finalImage)
+            cv2.rectangle(origPic, mousePoint[0], mousePoint[1], (0, 255, 0), 10)
+    	cv2.imshow("Original", origPic)
         ### Show Images END ###
 
+        ### Centroid Calculations ###
+        M = cv2.moments(cnt)
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        firstCx = cx - 10
+        firstCy = cy - 10
+        secondCx = cx + 10
+        secondCy = cy + 10
+        #print(cx,firstCx, secondCx)
+        #print(cx, secondCx, secondCy)
+        cv2.rectangle(altColorSpaceImg, (firstCx,firstCy), (secondCx,secondCy), (255,0,0), 10)
+        cv2.imshow('Alternative Color Space Image', altColorSpaceImg)
         # Note: waitKey() actually pushes the image out to screen
         if cv2.waitKey(1) ==27:
             exit(0)
