582c1826f7903c88933c687fa35ba2e3cb07cb56
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2017-12-03 15:46:56 -0700

    Can't figure out this weird var thing

--- KaiVid/learningCV.py
+++ KaiVid/learningCV.py
@@ -2,26 +2,38 @@ from base.KaicongInput import KaicongInput
 import cv2
 import numpy as np
 
+# Color space info:
+# Good: YUV LUV
+# Okay: HSV HLS
+# Bad: RGB BGR
+
 ### Variables for click_and_crop ###
 mousePoint = []
+boardPoint = []
 cropping = False
 ### Variables for click_and_crop ###
-
-# test change
-
+ix, iy = -1, -1
 class KaicongVideo(KaicongInput):
-    global maxContour, maxContourData
+    global maxContour, maxContourData, ix, iy
     maxContour = 0
     maxContourData = 0
     ### Mouse point Drawing ###
     def click_and_crop(event, x, y, flags, param):
-    	global mousePoint, cropping
+    	global mousePoint, cropping, boardPoints
     	if event == cv2.EVENT_LBUTTONDOWN:
-    		print('foo')
     		mousePoint = [(x, y)]
     		cropping = True
     		mousePoint.append((x, y))
     		cropping = False
+
+        if event == cv2.EVENT_RBUTTONDOWN:
+            print 'Defined Board Position'
+            boardPoint = [(x, y)]
+            cropping = True
+            boardPoint.append((x, y))
+            cropping = False
+            ix,iy = x,y
+            print(boardPoint)
 
     cv2.namedWindow("Original")
     cv2.setMouseCallback("Original", click_and_crop)
@@ -63,16 +75,16 @@ if __name__ == "__main__":
 
     def show_video(jpg):
         ### Defining Inital Necessary Variables ###
+        redUpper = np.array([120, 150, 255], dtype=np.uint8) #Thresholds for robot ID
         redLower = np.array([0, 0, 100], dtype=np.uint8) #Thresholds for robot ID
         #redUpper = np.array([120, 150, 255], dtype=np.uint8) #Thresholds for robot ID
-        redUpper = np.array([120, 150, 255], dtype=np.uint8) #Thresholds for robot ID
         kernel = np.ones((5,5), np.uint8)
 
         readColors = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.IMREAD_COLOR)
         origPic = readColors
-        #HSV Works really well here, currenty sets everything red to white
+        #YUV and LUV Work really well here, currenty sets everything robot to white
         #Else set to black
-        altColorSpaceImg = cv2.cvtColor(readColors, cv2.COLOR_BGR2YUV) #Converts to YUV
+        altColorSpaceImg = cv2.cvtColor(readColors, cv2.COLOR_BGR2LUV) #Converts to LUV
 
         blurredImg = cv2.GaussianBlur(altColorSpaceImg, (11, 11), 10) #Blurs image to deal with noise
         #blurredImg = cv2.medianBlur(blurredImg, 5) #Uses median blur to filter out even more noise
@@ -85,22 +97,24 @@ if __name__ == "__main__":
 
         ### Contour Stuff ###
         im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #Find countour for masked image
-        cnt = contours[0]
+        # Try catch for contour issues on other color spaces
+        try:
+            cnt = contours[1]
+        except IndexError:
+            cnt = contours[0]
+
         cv2.drawContours(altColorSpaceImg, contours, -1, (0,0,255), 2) #Draw countours on ALT Image
         # Finds Largest blob
 
         for contour in contours:
+            global maxContour, contourSize, maxContourData
             contourSize = cv2.contourArea(cnt)
-            global maxContour
             if contourSize > maxContour:
-                print('big')
-                global maxContour, maxContourData
                 maxContour = contourSize
+                #print(contour)
                 maxContourData = contour
 
         areaMask = np.zeros_like(mask)
-
-
         cv2.fillPoly(areaMask,[maxContourData],1) # Draws new areaMask onto new image
 
         R,G,B = cv2.split(blurredImg) #Splits image in to RGB Values
@@ -112,17 +126,22 @@ if __name__ == "__main__":
         # Contour Stuff End ###
 
         ### Show Images ###
-        cv2.imshow('Final',finalImage)
+        #cv2.imshow('Final',finalImage)
         cv2.imshow('Mask', mask)
-
-        if mousePoint == []:
-            cv2.imshow('Original', origPic)
-        else:
-            cv2.rectangle(origPic, mousePoint[0], mousePoint[1], (0, 255, 0), 10)
-    	cv2.imshow("Original", origPic)
+        cv2.imshow('Alternative Color Space Image', altColorSpaceImg)
         ### Show Images END ###
 
         ### Centroid Calculations ###
+
+        if mousePoint == [] and boardPoint == []:
+            cv2.imshow('Original', origPic)
+        else:
+            global ix, iy
+            cv2.rectangle(origPic, mousePoint[0], mousePoint[1], (0, 255, 0), 10)
+            cv2.rectangle(origPic, boardPoint[0], boardPoint[1], (0, 255, 0), 10)
+            cv2.imshow("Original", origPic)
+
+
         M = cv2.moments(cnt)
         cx = int(M['m10']/M['m00'])
         cy = int(M['m01']/M['m00'])
@@ -130,10 +149,9 @@ if __name__ == "__main__":
         firstCy = cy - 10
         secondCx = cx + 10
         secondCy = cy + 10
-        #print(cx,firstCx, secondCx)
-        #print(cx, secondCx, secondCy)
-        cv2.rectangle(altColorSpaceImg, (firstCx,firstCy), (secondCx,secondCy), (255,0,0), 10)
-        cv2.imshow('Alternative Color Space Image', altColorSpaceImg)
+        # Draw Centorid
+        cv2.rectangle(origPic, (firstCx,firstCy), (secondCx,secondCy), (255,0,0), -1)
+        cv2.imshow('Original', origPic)
         # Note: waitKey() actually pushes the image out to screen
         if cv2.waitKey(1) ==27:
             exit(0)
