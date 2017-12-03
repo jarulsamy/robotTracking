ffd060f3ba8ed9bac96ad6f45c75e833967944bd
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2017-12-02 21:12:40 -0700

    Small bug fixes

--- KaiVid/learningCV.py
+++ KaiVid/learningCV.py
@@ -2,28 +2,47 @@ from base.KaicongInput import KaicongInput
 import cv2
 import numpy as np
 
-maxCountour = 0 #DO NOT DELETE ME
+# Color space info:
+# Good: YUV LUV YCR_CV
+# Okay: HSV HLS LAB Most of these have issues with noise
+# Bad: RGB BGR
 
 ### Variables for click_and_crop ###
 mousePoint = []
+boardPoint = []
 cropping = False
+ix, iy = -1, -1
 ### Variables for click_and_crop ###
 
-
 class KaicongVideo(KaicongInput):
+    ### Initial Variable Decleration ###
+    global maxContourChassis, maxContourDataChassis, maxContourBoard, maxContourDataBoard, ix, iy
+    maxContourChassis = 0
+    maxContourDataChassis = 0
+    maxContourBoard = 0
+    maxContourDataBoard = 0
+    ### Initial Variable Decleration ###
 
     ### Mouse point Drawing ###
     def click_and_crop(event, x, y, flags, param):
-    	global mousePoint, cropping
+    	global mousePoint, cropping, boardPoints
     	if event == cv2.EVENT_LBUTTONDOWN:
-    		print('foo')
     		mousePoint = [(x, y)]
     		cropping = True
     		mousePoint.append((x, y))
     		cropping = False
 
-    cv2.namedWindow("Final")
-    cv2.setMouseCallback("Final", click_and_crop)
+        if event == cv2.EVENT_RBUTTONDOWN:
+            print 'Defined Board Position'
+            boardPoint = [(x, y)]
+            cropping = True
+            boardPoint.append((x, y))
+            cropping = False
+            ix,iy = x,y
+            print(boardPoint)
+
+    cv2.namedWindow("Original")
+    cv2.setMouseCallback("Original", click_and_crop)
     ### Mouse Point Drawing End ###
 
     PACKET_SIZE = 1024
@@ -62,60 +81,134 @@ if __name__ == "__main__":
 
     def show_video(jpg):
         ### Defining Inital Necessary Variables ###
-        redLower = np.array([0, 0, 100], dtype=np.uint8) #Thresholds for robot ID
+        redUpper = np.array([110, 150, 255], dtype=np.uint8) #Thresholds for chassis ID
+        redLower = np.array([0, 0, 100], dtype=np.uint8) #Thresholds for chassis ID
+
+        greenUpper = np.array([200, 200, 255], dtype=np.uint8) #Thresholds for board ID
+        greenLower = np.array([0, 0, 150], dtype=np.uint8) #Thresholds for board ID
+
         #redUpper = np.array([120, 150, 255], dtype=np.uint8) #Thresholds for robot ID
-        redUpper = np.array([120, 150, 255], dtype=np.uint8) #Thresholds for robot ID
         kernel = np.ones((5,5), np.uint8)
 
         readColors = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.IMREAD_COLOR)
-        #HSV Works really well here, currenty sets everything red to white
+        origPic = readColors
+        #YUV and LUV Work really well here, currenty sets everything robot to white
         #Else set to black
-        altColorSpaceImg = cv2.cvtColor(readColors, cv2.COLOR_BGR2YUV) #Converts to YUV
-        blurredImg = cv2.GaussianBlur(altColorSpaceImg, (11, 11), 10) #Blurs image to deal with noise
-        #blurredImg = cv2.medianBlur(blurredImg, 5) #Uses median blur to filter out even more noise
-        blurredImg = cv2.bilateralFilter(blurredImg, 25, 75, 75) #Uses bilaterial filtering to deal with more noise
+        chassisImg = cv2.cvtColor(readColors, cv2.COLOR_BGR2LUV) #Converts to LUV for chassis detectionS
+        boardImg = cv2.cvtColor(readColors, cv2.COLOR_BGR2XYZ) # Converts to HLS for board detection
+
+        blurredImgChassis = cv2.GaussianBlur(chassisImg, (11, 11), 10) #Blurs image to deal with noise
+        blurredImgChassis = cv2.bilateralFilter(blurredImgChassis, 25, 75, 75) #Uses bilaterial filtering to deal with more noise
+
+        blurredImgBoard = cv2.GaussianBlur(boardImg, (11, 11), 10) #Blurs image to deal with noise
+        blurredImgBoard = cv2.bilateralFilter(blurredImgBoard, 25, 75, 75) #Uses bilaterial filtering to deal with more noise
 
         ### Mask Stuff ###
-        mask = cv2.inRange(blurredImg, redLower, redUpper)
-    	mask = cv2.erode(mask, kernel, iterations=2)
-    	mask = cv2.dilate(mask, kernel, iterations=2)
+        maskChassis = cv2.inRange(blurredImgChassis, redLower, redUpper)
+    	maskChassis = cv2.erode(maskChassis, kernel, iterations=2)
+    	maskChassis = cv2.dilate(maskChassis, kernel, iterations=2)
+
+        maskBoard = cv2.inRange(blurredImgBoard, greenLower, greenUpper)
+    	maskBoard = cv2.erode(maskBoard, kernel, iterations=2)
+    	maskBoard = cv2.dilate(maskBoard, kernel, iterations=2)
         ### Mask Stuff END ###
 
         ### Contour Stuff ###
-        im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #Find countour for masked image
-        cnt = contours[0]
-        cv2.drawContours(altColorSpaceImg, contours, -1, (0,0,255), 2) #Draw countours on ALT Image
+        im2Chassis, contoursChassis, hierarchyChassis = cv2.findContours(maskChassis, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #Find countour for masked image
+        im2Board, contoursBoard, hierarchyBoard = cv2.findContours(maskBoard, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #Find countour for masked image
+        # Try catch for contour issues on other color spaces
+        try:
+            cntChassis = contoursChassis[1]
+        except IndexError:
+            cntChassis = contoursChassis[0]
+
+        try:
+            cntBoard = contoursBoard[1]
+        except IndexError:
+            cntBoard = contoursBoard[0]
+
+
+        cv2.drawContours(chassisImg, contoursChassis, -1, (0,0,255), 2) #Draw countours on ALT Image
+        cv2.drawContours(boardImg, contoursBoard, -1, (0,0,255), 2) #Draw countours on ALT Image
         # Finds Largest blob
 
-        for contour in contours:
-            contourSize = cv2.contourArea(cnt)
-            if contourSize > maxCountour:
-                maxContour = contourSize
-                maxContourData = contour
+        for contourChassis in contoursChassis:
+            global maxContourChassis, contourSizeChassis, maxContourDataChassis
+            contourSizeChassis = cv2.contourArea(cntChassis)
+            if contourSizeChassis > maxContourChassis:
+                maxContourChassis = contourSizeChassis
+                maxContourDataChassis = contourChassis
+            # Has issues if put togethor
+        for contourBoard in contoursBoard:
+            global maxContourBoard, contourSizeBoard, maxContourDataBoard
+            contourSizeBoard = cv2.contourArea(cntBoard)
+            if contourSizeBoard > maxContourBoard:
+                maxContourBoard = contourSizeBoard
+                maxContourDataBoard = contourBoard
 
-        areaMask = np.zeros_like(mask)
+        areaMaskChassis = np.zeros_like(maskChassis)
+        areaMaskBoard = np.zeros_like(maskBoard)
 
-        cv2.fillPoly(areaMask,[maxContourData],1) # Draws new areaMask onto new image
+        cv2.fillPoly(areaMaskChassis,[maxContourDataChassis],1) # Draws new areaMask onto new image
+        cv2.fillPoly(areaMaskBoard,[maxContourDataBoard],1) # Draws new areaMask onto new image
 
-        R,G,B = cv2.split(blurredImg) #Splits image in to RGB Values
+        # Somewhat unnecessary and problomatic code, can be reworked in the future
+        Rc,Gc,Bc = cv2.split(blurredImgChassis) #Splits image in to RGB Values
+        Rb,Gb,Bb = cv2.split(blurredImgBoard) #Splits image in to RGB Values
         # Creates solid black image + mask
-        finalImage = np.zeros_like(blurredImg)
-        finalImage[:,:,0] = np.multiply(R,areaMask)
-        finalImage[:,:,1] = np.multiply(G,areaMask)
-        finalImage[:,:,2] = np.multiply(B,areaMask)
+        finalImage = np.zeros_like(blurredImgChassis)
+        finalImage[:,:,0] = np.multiply(Rc,areaMaskChassis)
+        finalImage[:,:,1] = np.multiply(Gc,areaMaskChassis)
+        finalImage[:,:,2] = np.multiply(Bc,areaMaskChassis)
+
+        finalImage[:,:,0] = np.multiply(Rb,areaMaskBoard)
+        finalImage[:,:,1] = np.multiply(Gb,areaMaskBoard)
+        finalImage[:,:,2] = np.multiply(Bb,areaMaskBoard)
         # Contour Stuff End ###
 
         ### Show Images ###
-        cv2.imshow('Mask',mask)
-        cv2.imshow('Alternative Color Space Image', altColorSpaceImg)
         #cv2.imshow('Final',finalImage)
+        cv2.imshow('Chassis Mask', maskChassis)
+        cv2.imshow('Chassis Image', chassisImg)
+        cv2.imshow('Board Image', boardImg)
+        ### Show Images END ###
+
+        ### Centroid Calculations ###
 
         if mousePoint == []:
-            cv2.imshow('Final',finalImage)
+            cv2.imshow('Original', origPic)
         else:
-            cv2.rectangle(finalImage, mousePoint[0], mousePoint[1], (0, 255, 0), 2)
-    	cv2.imshow("Final", finalImage)
-        ### Show Images END ###
+            cv2.rectangle(origPic, mousePoint[0], mousePoint[1], (0, 255, 0), 10)
+            cv2.imshow("Original", origPic)
+
+        if boardPoint == []:
+            cv2.imshow('Original', origPic)
+        else:
+            cv2.rectangle(origPic, boardPoint[0], boardPoint[1], (0, 255, 0), 10)
+            cv2.imshow("Original", origPic)
+
+
+
+        mChassis = cv2.moments(cntChassis)
+        mBoard = cv2.moments(cntBoard)
+        cxC = int(mChassis['m10']/mChassis['m00'])
+        cyC = int(mChassis['m01']/mChassis['m00'])
+
+        cxB = int(mBoard['m10']/mBoard['m00'])
+        cyB = int(mBoard['m01']/mBoard['m00'])
+        # Centroid Range Chassis
+        firstCxC = cxC - 10
+        firstCyC = cyC - 10
+        secondCxC = cxC + 10
+        secondCyC= cyC + 10
+        # Centroid Range Board
+        firstCxB = cxB - 10
+        firstCyB = cyB - 10
+        secondCxB = cxB + 10
+        secondCyB = cyB + 10
+        # Draw Centorid
+        cv2.rectangle(origPic, (firstCxC,firstCyC), (secondCxC,secondCyC), (255,0,0), -1) # Draws Centroid Chassis
+        cv2.rectangle(origPic, (firstCxB,firstCyB), (secondCxB,secondCyB), (0,0,255), -1) # Draws Centroid Board
 
         # Note: waitKey() actually pushes the image out to screen
         if cv2.waitKey(1) ==27:
