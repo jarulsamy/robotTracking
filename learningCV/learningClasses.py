f2e3657effcbe5d704aa3d28f68ef6d7bd9068d5
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2017-12-16 14:17:05 -0700

    Add files via upload
    I learned how to use classes!

--- learningCV/learningClasses.py
+++ learningCV/learningClasses.py
@@ -2,20 +2,25 @@
 
 import sys
 
-import cv2
-import numpy as np
-
 class robotTracking(object):
+# Variables defined here can be used when the class is called with that given name.
+# In this case math = robotTracking() so for example, print(math.newThing)
+    def __init__(self):
+        print('foo')
+        self.newThing = 'foo'
+    def add2Things(self, a, b):
+        self.newThing = a + b
+        return self.newThing
+    def sub2Things(self, a, b):
+        self.newThing = a - b
+        return self.newThing
+    def x2Things(self, a, b):
+        self.newThing = a * b
+        return self.newThing
+    def divide2Things(self, a, b):
+        self.newThing = a / b
+        return self.newThing
 
-    def __init__(self, img):
-        self.img = img
-
-    def show_img(self):
-        cv2.imshow('foo', self.img)
-        cv2.waitKey(0)
-
-    def findContours(self):
-        
-img = cv2.imread('img.jpg')
-jeff = robotTracking(cv2.imread('img.jpg'))
-jeff.show_img()
+math = robotTracking()
+print(math.sub2Things(3, 4))
+print(math.newThing)
