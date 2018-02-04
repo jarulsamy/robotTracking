c75b55ac9769af08d1f270771cb985c0fcd3aa7f
Joshua <joshua.gf.arul@gmail.com>
2018-02-03 22:03:12 -0700

    Fixed spacing errors and cleaned up unused variables
    Former-commit-id: 6812e0ea1d765dfee1c15eb291f9b9a4f1aa421a

--- KaiVid/KaicongMotor.py
+++ KaiVid/KaicongMotor.py
@@ -2,7 +2,7 @@ from base.KaicongOutput import KaicongOutput
 import urllib2
 
 class KaicongMotor(KaicongOutput):
-    
+
     # These are the original state commands to send to the Kaicong camera.
     CMDLIST = {
         "PTZ_UP": 0,
@@ -26,73 +26,74 @@ class KaicongMotor(KaicongOutput):
         "IO_ON": 94, # TODO: What does this do?
         "IO_OFF": 95, # and this one?
     }
-    
+
     # This table converts a vector-style direction to its command
     MOVELIST = {
         "00": "PTZ_STOP",
-        
+
         "0+": "PTZ_UP",
         "0-": "PTZ_DOWN",
-        
+
         "+0": "PTZ_RIGHT",
         "-0": "PTZ_LEFT",
-        
+
         "++": "PTZ_RIGHT_UP",
         "+-": "PTZ_RIGHT_DOWN",
         "-+": "PTZ_LEFT_UP",
         "--": "PTZ_LEFT_DOWN",
     }
-    
+
     URI = "http://{0}:81/decoder_control.cgi?loginuse={1}&loginpas={2}&command=%d&onestep=0"
 
     def __init__(self, domain, user="admin", pwd="123456"):
         KaicongOutput.__init__(
-            self, 
-            domain, 
-            KaicongMotor.URI, 
-            user, 
+            self
+            domain,
+            KaicongMotor.URI,
+            user,
             pwd
         )
-        
+
         self.state = '00'
-        
-    def _to_symbol(self, v):
+
+    @classmethod
+    def to_symbol(self, v):
         if v > 0:
             return '+'
         if v < 0:
             return '-'
         else:
             return '0'
-    
-    def send_command(self, cmdstr): 
+
+    def send_command(self, cmdstr):
         stream = urllib2.urlopen(self.uri % (KaicongMotor.CMDLIST[cmdstr]))
         result = stream.read()
         assert "ok" in result
         stream.close()
-        
+
     def move(self, xy):
-        move_symbol = self._to_symbol(xy[0]) + self._to_symbol(xy[1])
+        move_symbol = self.to_symbol(xy[0]) + self.to_symbol(xy[1])
         cmdstr = KaicongMotor.MOVELIST[move_symbol]
         if cmdstr != self.state:
             self.send_command(cmdstr)
         self.state = cmdstr
-        
-    
-        
-        
+
+
+
+
 if __name__ == "__main__":
     import pygame
     import sys
-    
+
     if len(sys.argv) != 2:
         print "Usage: %s <ip_address>" % sys.argv[0]
         sys.exit(-1)
-    
+
     pygame.init()
     screen = pygame.display.set_mode((320, 240))
-    
+
     motor = KaicongMotor(sys.argv[1])
-    
+
     def checkKeys():
         keys = pygame.key.get_pressed()
         x = 0
@@ -105,13 +106,11 @@ if __name__ == "__main__":
             x = 1
         if keys [pygame.K_w]:
             y = 1
-            
+
         motor.move([x, y])
-            
+
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                  sys.exit()
         checkKeys()
-        
-        
