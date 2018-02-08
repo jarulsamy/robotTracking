dd33f935b2c14bce04c45950b0a1a69530c7afea
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2018-02-07 17:04:48 -0700

    Finally figured out byte issue
    Former-commit-id: 1f99d04e9eb909700734748da75ae14c7a439a9f

--- Robot/bytes.py
+++ Robot/bytes.py
@@ -1,28 +1,32 @@
 import serial
 from time import sleep
+#
+# ser = serial.Serial(
+#     port = "COM3",
+#     baudrate = 38400
+#     # baudrate = 115200,
+#     # stopbits = serial.STOPBITS_ONE
+# )
 
-ser = serial.Serial(
-    port = "COM3",
-    baudrate = 115200,
-    stopbits = serial.STOPBITS_ONE
-)
+
+with serial.Serial("COM3", 115200, timeout = 1) as ser:
 
 
-print(ser.isOpen())
+    print(ser.isOpen())
 
-ledOn = "\x7e"
-ledOn1 = "\xff"
-ledOff = "\x7e"
-ledOff1 = "\x00"
-# ledOn = "\xff"
-# ledOff = "\x00"
+    ledOn = "\x7e\xff"
+    # ledOn1 = ""
+    ledOff = "\x7e\x00"
+    # ledOff1 = ""
+    # ledOn = "\xff"
+    # ledOff = "\x00"
+    # ser.readTimeout = 1
+    ser.write(ledOn)
+    # readOne = ser.read(100)
+    sleep(1)
+    ser.write(ledOff)
+    readTwo = ser.readline()
+    # print(readOne)
+    print(readTwo)
 
-ser.write(ledOn)
-ser.write(ledOn1)
-print(ser.read())
-sleep(1)
-ser.write(ledOff)
-ser.write(ledOff1)
-print(ser.read())
-
-ser.close()
+    ser.close()
