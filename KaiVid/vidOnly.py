34bbc7213ec2359b6d4146b7145ca3a4416e16d2
= <joshua.gf.arul@gmail.com>
2018-03-20 08:16:52 -0600

    Removed unused test files. Added vidOnly version. Need to update readme
    Former-commit-id: 972b80f97dab597606093ba836d2489c4de830c9

--- KaiVid/vidOnly.py
+++ KaiVid/vidOnly.py
@@ -1,18 +1,11 @@
-### Usage: python findRobot.py IP_ADDRESS
+### Usage: python vidOnly.py IP_ADDRESS
 
 from base.KaicongInput import KaicongInput
-import socket
 import sys
 import numpy as np
 import cv2
 import math
 import time
-
-### Host and Port info for pipeline server ###
-HOST = '127.0.0.1'
-PORT = 10000
-s = socket.socket()
-s.connect((HOST, PORT)) # Comment me if no robot untested and may have some issues. Submit a pull request if there is a problem
 # Initial Variable Decleration
 pt = []
 centroidChassis = []
@@ -89,9 +82,8 @@ if __name__ == "__main__":
         edgeChassis = cv2.Canny(maskChassis, 75, 200) # Runs cv2.canny to give us better contours
         edgeBoard = cv2.Canny(maskBoard, 75, 200) # Runs cv2.canny to give us better contours
 
-        im2Chassis, contoursChassis, hierarchyChassis = cv2.findContours(edgeChassis, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #Find countour for masked image
-        # im2Board, contoursBoard, hierarchyBoard = cv2.findContours(maskBoard, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #Find countour for masked image
-        im2Board, contoursBoard, hierarchyBoard = cv2.findContours(edgeBoard, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #Find countour for masked image
+        im2Chassis, contoursChassis, hierarchyChassis = cv2.findContours(edgeChassis, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #Find countour for masked chassisimage
+        im2Board, contoursBoard, hierarchyBoard = cv2.findContours(edgeBoard, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #Find countour for masked borad image
 
         cv2.drawContours(chassisImg, contoursChassis, -1, (0,0,255), 2) #Draw countours on alternate color space chassis image
         cv2.drawContours(boardImg, contoursBoard, -1, (0,0,255), 2) #Draw countours on alternate color space board image
@@ -124,9 +116,8 @@ if __name__ == "__main__":
         # All centroid calculations use the picked contours #
         for contours in contour_list_chassis:
             mChassis = cv2.moments(contours)
-            global cxC, cyC
-            cxC = int(mChassis['m10']/mChassis['m00'])
-            cyC = int(mChassis['m01']/mChassis['m00'])
+            cxC = int(mChassis['m10']/mChassis['m00']) #Centroid Calculation for x chassis
+            cyC = int(mChassis['m01']/mChassis['m00']) #Centroid Calculation for y chassis
             cv2.circle(origPic, (cxC,cyC), 10, (255,0,0), -20) # Draws Centroid Chassis
             global centroidChassis
             centroidChassis = [cxC, cyC]
@@ -188,76 +179,12 @@ if __name__ == "__main__":
             dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
             return dist
 
-        ### Movement / Sending to Robot ###
-        def lookAtPoint(facing):
-            if facing == False:
-                s.send('motors(0.1, -0.1)')
-                time.sleep(.05)
-                s.send('stop')
-
-        if pt != [] and centroidChassis != [] and centroidBoard != []:
-            # If all three items are defined, keep doing calculations. Else: Keep moving a little bit to hopefully reobtain the centroid
-            chassisDistPoint = calculateDistance(centroidChassis[0], centroidChassis[1], pt[0], pt[1])
-            boardDistPoint = calculateDistance(centroidBoard[0], centroidBoard[1], pt[0], pt[1])
-
-        if pt != []:
-            if chassisDistPoint < 50: # If statement to kill program after reaching a certain proximety to the point
-                # break
-                s.send("stop()")
-                sys.exit()
-                return 'end'
-            # Kind of unnecessary but will be added on to in the future
-            if boardDistPoint == chassisDistPoint:
-                print 'this should never happen, probably a centroid glitch'
-            if robotDirection != robotVPoint: # Turn until approx. face the clicked point
-                lookAtPoint(False)
-                if centroidBoard[0] > pt[0]: # All of this is intuative movement code, that doesn't really work
-                    s.send('motors(-.1, .1)')
-                    time.sleep(.05)
-                elif centroidBoard[0] < pt[0]:
-                    s.send('motors(.1, -.1)')
-                    time.sleep(.05)
-                elif centroidBoard[1] > pt[1]:
-                    s.send('motors(-.1, .1)')
-                    time.sleep(.05)
-                elif centroidBoard[1] < pt[1]:
-                    s.send('motors(.1, -.1)')
-                    time.sleep(.05)
-                else:
-                    s.send('stop()')
-                    time.sleep(.05)
-                    s.send('forward(.1,.1)')
-            else:
-                s.send('stop()')
-                time.sleep(.05)
-                s.send('forward(.1,.1)')
-
         # Show all the images / update all the images
         cv2.imshow('Original', origPic)
         cv2.imshow('Chassis Image', chassisImg)
         cv2.imshow('Board Image', boardImg)
-        cv2.imshow('Blurred Board Image', blurredImgBoard)
-        #
-        def goToPoint(click):
-            click = click[0]
-            x = click[0]
-            y = click[1]
-            print(x, y)
-            s.send("turnLeft(1,1)")
-
-        def click_and_crop(event, x, y, flags, param):
-            global mousePoint, cropping
-            if event == cv2.EVENT_LBUTTONUP:
-                mousePoint = [(x, y)]
-                mousePoint.append((x, y))
-                goToPoint(mousePoint)
-
-        cv2.namedWindow("Original")
-        cv2.setMouseCallback("Original", click_and_crop)
 
         if cv2.waitKey(1) == 27: # Close the windows and kill the program with Esc Key
-            s.send("stop()")
-            s.close()
             exit(0)
 
     video = KaicongVideo(sys.argv[1], show_video)
