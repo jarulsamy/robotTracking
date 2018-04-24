0ad19ccd6dc8e851be4b2f274a30a8ced7a46204
DESKTOP-76FAURK\Joshua <joshua.gf.arul@gmail.com>
2018-04-23 22:09:39 -0600

    Almost done with orientation issue
    Former-commit-id: 461c5ee3b3ba5bcec0ec3a0e24dda0c24e789c2c

--- WIP/orientation/angle.py
+++ WIP/orientation/angle.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 import cv2
 
-pic1 = cv2.imread("1.jpg")
+pic1 = cv2.imread("2.jpg")
 # pic2 = cv2.imread("2.jpg")
 
 redUpper = np.array([80, 40,  255], dtype=np.uint8) # Upper threshold for chassis ID # HSV VERSION
@@ -11,17 +11,6 @@ redLower = np.array([0, 20, 0], dtype=np.uint8) #Lower threshold for chassis ID 
 blueUpper = np.array([255, 180,  10], dtype=np.uint8) # Upper threshold for chassis ID # HSV VERSION
 blueLower = np.array([230, 160, 0], dtype=np.uint8) #Lower threshold for chassis ID # HSV VERSION
 
-
-
-maskRed = cv2.inRange(pic1, redLower, redUpper)
-edgeRed = cv2.Canny(maskRed, 75, 200)
-im2, contoursRed, hierarchy = cv2.findContours(edgeRed, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(pic1, contoursRed, -1, (0,255,0), 2)
-
-maskBlue = cv2.inRange(pic1, blueLower, blueUpper)
-edgeBlue = cv2.Canny(maskBlue, 75, 200)
-im2, contoursBlue, hierarchy = cv2.findContours(edgeBlue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(pic1, contoursBlue, -1, (0,0,255),2)
 
 # for contours in contoursRed:
 #     mRed = cv2.moments(contours)
