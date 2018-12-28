6b45d54e46d09153d279def8aff684b52eca6e5e
JoshuaA9088 <joshua.gf.arul@gmail.com>
2018-12-28 15:26:43 -0700

    Reworked detection drawing methodolgy to draw ALL scores above 50% not just first 2

---
+++ tensorflow/fpsTest.py
@@ -0,0 +1,12 @@
+import cv2
+
+v = cv2.VideoCapture("http://10.0.0.101:8000/stream.mjpg")
+ret, frame = v.read()
+
+while True:
+    ret, frame = v.read()
+    cv2.imshow('img', frame)
+    fps = v.get(cv2.CAP_PROP_FPS)
+    print "Frames per second using: {0}".format(fps)
+    if cv2.waitKey(1) == 27:
+        exit(0)
