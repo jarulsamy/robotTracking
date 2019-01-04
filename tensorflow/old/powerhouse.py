3fe07372cd4c5d5988611a42ad4ec1700b94a594
joshuaa9088 <joshua.gf.arul@gmail.com>
2019-01-03 21:46:03 -0700

    Removed many old unnecessay files

---
+++ tensorflow/old/powerhouse.py
@@ -0,0 +1,224 @@
+import tensorflow as tf
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+
+from collections import defaultdict
+from io import StringIO
+
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+import cv2
+import threading
+import time
+
+import platform
+
+CWD_PATH = os.getcwd()
+MODEL_NAME = 'scribbler_graph_board_v3/'
+PATH_TO_CKPT = '{}frozen_inference_graph.pb'.format(MODEL_NAME)
+# PATH_TO_CKPT = '{}opt_graph.pb'.format(MODEL_NAME)
+PATH_TO_LABELS = 'object-detection.pbtxt'
+
+pt = []
+
+NUM_CLASSES = 2
+
+
+class imageThread(threading.Thread):
+
+    def __init__(self, threadID, name, URL):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.url = URL
+        self.cap = cv2.VideoCapture(URL)
+        # self.cap = cv2.VideoCapture("stream.avi")
+        # self.cap = cv2.VideoCapture(0)
+
+    def getFrame(self):
+        ret, frame = self.cap.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        # frame = self.grayscaleify(frame)
+        return frame
+
+    def grayscaleify(self, image):
+        # The function supports only grayscale images
+        assert len(image.shape) == 2, "Not a grayscale input image"
+        last_axis = -1
+        dim_to_repeat = 2
+        repeats = 3
+        grscale_img_3dims = np.expand_dims(image, last_axis)
+        training_image = np.repeat(
+            grscale_img_3dims, repeats, dim_to_repeat).astype('uint8')
+        assert len(training_image.shape) == 3
+        assert training_image.shape[-1] == 3
+        return training_image
+
+
+# Load Detection graph
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+# Load label map
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(
+    label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+IMAGE_SIZE = (12, 8)
+
+URL = "http://10.0.0.101:8000/stream.mjpg"
+# URL = "stream.avi"
+# Setup thread to grab / convert color spaces of images
+imgThread = imageThread(1, 'image', URL)
+imgThread.daemon = True
+imgThread.start()
+
+
+def click(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        global pt
+        pt = [x, y]
+    return pt
+
+### PERFORAMCNE TUNING CPU ONLY###
+
+
+# Auto detect platform and configure core counts if necessary
+# if "Intel64" in platform.processor():
+threads = 4
+print("Intel CPU Detected...")
+print("Attempting to configure Intel MKL DNN...")
+config = tf.ConfigProto()
+# SHOULD ALWAYS BE SAME AS OMP_NUM_THREADS
+config.intra_op_parallelism_threads = threads
+config.inter_op_parallelism_threads = threads
+os.environ["OMP_NUM_THREADS"] = str(threads)
+os.environ["KMP_BLOCKTIME"] = str(threads)
+os.environ["KMP_SETTINGS"] = "0"
+os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact,1,0"
+print("Successfully loaded Intel MKL Settings")
+
+# elif "AMD" in platform.processor():
+#     threads = 12
+#     print("AMD CPU Detected...")
+#     print("Attempting to configure MKL DNN Library...")
+#     config = tf.ConfigProto()
+#     config.intra_op_parallelism_threads = threads
+#     config.inter_op_parallelism_threads = threads
+#     os.environ["OMP_NUM_THREADS"] = str(threads)
+#     os.environ["KMP_BLOCKTIME"] = str(threads)
+#     os.environ["KMP_SETTINGS"] = "0"
+#     os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact,1,0"
+#     print("Successfully loaded Intel MKL Settings")
+
+# else:
+config = tf.ConfigProto()
+cluster = tf.train.ClusterSpec({"local": ["10.0.0.3:2222", "10.0.0.3:2223"]})
+
+with detection_graph.as_default():
+    with tf.Session("grpc://10.0.0.3:2222", graph=detection_graph) as sess:
+        while True:
+            startTime = time.time()
+            image_np = imgThread.getFrame()  # Grab image
+
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+            boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name(
+                'num_detections:0')
+
+            (boxes, scores, classes, num_detections) = sess.run(
+                [boxes, scores, classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
+
+            height, width, channels = image_np.shape
+
+            boxes = np.squeeze(boxes)
+            classes = np.squeeze(classes)
+            scores = np.squeeze(scores)
+
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                image_np,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                min_score_thresh=.3,
+                line_thickness=5)
+
+            # Chassis Centroid
+            if scores[0] > .5:
+                box = tuple(boxes[0].tolist())
+                yMin = box[0] * height
+                xMin = box[1] * width
+                yMax = box[2] * height
+                xMax = box[3] * width
+
+                xCenter = (xMax + xMin) / 2
+                yCenter = (yMax + yMin) / 2
+
+                xCenterChassis = int(xCenter)
+                yCenterChassis = int(yCenter)
+
+                cv2.circle(image_np, (xCenterChassis, yCenterChassis),
+                           10,  (255, 0, 0), -1)
+
+            # QR Code Centroid
+            if scores[1] > .5:
+                box = tuple(boxes[1].tolist())
+                yMin = box[0] * height
+                xMin = box[1] * width
+                yMax = box[2] * height
+                xMax = box[3] * width
+
+                xCenter = (xMax + xMin) / 2
+                yCenter = (yMax + yMin) / 2
+
+                xCenterQr = int(xCenter)
+                yCenterQr = int(yCenter)
+
+                cv2.circle(image_np, (xCenterQr, yCenterQr),
+                           10,  (0, 0, 255), -1)
+
+            image_np = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
+            if pt == []:
+                cv2.imshow('Detection', image_np)
+            else:
+                cv2.circle(image_np, (pt[0], pt[1]), 5,  (0, 255, 0), -1)
+                cv2.imshow("Detection", image_np)
+
+            cv2.namedWindow("Detection")
+            cv2.setMouseCallback("Detection", click)
+            elapsedTime = time.time() - startTime
+            print("Processing Time: {}".format(elapsedTime))
+            if cv2.waitKey(1) == 27:
+                exit(0)
+
+
+# cluster = tf.train.ClusterSpec({"local": ["10.0.0.3:2222", "10.0.0.3:2223"]})
+
+# x = tf.constant(2)
+
+
+
+# with tf.Session("grpc://10.0.0.3:2222") as sess:
+#     y2 = x - 66
+
+#     y1 = x + 300
+#     y = y1 + y2
+#     result = sess.run(y)
+#     print(result)
---
+++ tensorflow/old/qrDetect.py
@@ -0,0 +1,16 @@
+import cv2
+from pyzbar.pyzbar import decode
+
+URL = "http://10.0.0.101:8000/stream.mjpg"
+cap = cv2.VideoCapture(URL)
+
+while True:
+    ret, frame = cap.read()
+    qr = decode(frame)
+    for obj in qr:
+        poly = (obj.polygon)
+        cv2.rectangle(frame, poly[0], poly[2], (255, 0, 0), 3)
+    cv2.imshow("img", frame)
+
+    if cv2.waitKey(1) == 27:
+        exit(0)
