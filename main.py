c28e04c7c3bc6776cb0e4f1891b43ea4f315eb55
jarulsamy <joshua.gf.arul@gmail.com>
2020-03-30 00:32:30 -0600

    Reorganized structure.
    Removed unused files.
    Modularized.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import time
 
 import cv2
-from driver import orient
-from img import calibrate
-from img import ImageThread
-from img import mask
+from tracker import orient
+from tracker import calibrate
+from tracker import ImageThread
+from tracker import mask
+
+# For now, only CV2 color-based detection is supported and working properly.
 
 if __name__ == "__main__":
     URL = "http://Zeta.lan:8000/stream.mjpg"
+    COM_PORT = "COM5"
 
     image_thread = ImageThread(URL).start()
     time.sleep(0.1)
@@ -21,6 +24,7 @@ if __name__ == "__main__":
             frame, (cX, cY) = mask(frame, calibration)
         if front_coords is not None:
             cv2.circle(frame, front_coords, 7, (0, 255, 0), -1)
+        # If not calibrated, just show the image.
         cv2.imshow("frame", frame)
 
         # Handle all keypress events
@@ -41,7 +45,7 @@ if __name__ == "__main__":
             if calibrate is None:
                 print("Calibrate first!")
             else:
-                ret, front_coords = orient(image_thread, "frame", calibration, "COM5")
+                ret, front_coords = orient(image_thread, "frame", calibration, COM_PORT)
                 if ret != 0:
                     image_thread.stop()
                     exit(0)
